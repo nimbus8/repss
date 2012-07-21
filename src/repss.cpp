cc598b0ecf66dda7e00032471a6e6135f988d0db
khwillia <khalique.williams@gmail.com>
2012-07-21 05:52:08 -0400

    added some random tests

--- src/repss.cpp
+++ src/repss.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 #include "repss.hpp"
+#include "tests/test_repss_string.cpp"
 
-int main(int argc, const char* argv[]) {
-  std::cout << "REPSS" << endl;
+int main(int argc, char* argv[]) {
+  std::cout << "== REPSS ==\n" << endl;
+
+  vector<char> character_stream = { 'a', 'b', 'c', 'd', 'e', 'f' };
+  vector<char> output_stream;
+
+  auto i = 2;
+  auto lambda_func = [&output_stream,&i] (char x, int y) ->int {
+    output_stream.push_back(x+y+i);
+    return i+1;
+  };
+
+  std::cout << "(input-stream): " << std::endl;
+
+  for(auto v : character_stream)
+  {
+        std::cout << v << "\t";
+        i = lambda_func(v,1);
+  }
+
+  std::cout << "\n\n(output-stream): " << std::endl;
+
+  for(int k=0;k<output_stream.size();k++)
+    std::cout << output_stream[k] << "\t";
+
+  std::cout << std::endl << std::endl;
+
+  repss_str_test(argc,argv);
+
   return 0;
 }
--- src/tests/test_repss_string.cpp
+++ src/tests/test_repss_string.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 #include "../includes/repss_str.hpp"
 
-int main(int argc, char *argv[])
+int repss_str_test(int argc, char *argv[])
 {
-	if (argc != 3)
+	if (argc < 3)
 	{
-		printf("err, proper usage: test_repss_string str1 str2\n");
-		exit(1);
+	    char firstArg[] = "== repss-str-test ==\n";
+	    char secondArg[] = "adda";
+	    char thirdArg[] = "abbba";
+        char *argv2[] = {firstArg,secondArg,thirdArg};
+
+        argv = argv2;
 	}
 	
 	char a[] = "abc";
@@ -16,18 +20,21 @@ int main(int argc, char *argv[])
 	char str_percent[] = "%%";
 	char buffer[10];
 
+    cout << argv[0] << endl;
+
 	cout << b << "==" << a << ": " << repss_str::strcmp_ce(b,a) << " len(a): " << repss_str::strlen_ce(a) << endl;	
 	cout << buffer << "==" << str_percent << ": " << repss_str::strcmp_ce(buffer,str_percent) << endl;
 	buffer[0] = '%';
 	cout << buffer << "==" << str_percent << ": " << repss_str::strcmp_ce(buffer,str_percent) << endl;
 	cout << argv[1] << "==" << argv[2] << ": " << repss_str::strcmp_ce(argv[1], argv[2]) << ", len(a):" << repss_str::strlen_ce(argv[1]) << " len(b):" << repss_str::strlen_ce(argv[2]) << endl;
-        buffer[1] = '%';
-        cout << buffer << "==" << str_percent << ": " << repss_str::strcmp_ce(buffer,str_percent) << endl;
+    buffer[1] = '%';
+    cout << buffer << "==" << str_percent << ": " << repss_str::strcmp_ce(buffer,str_percent) << endl;
 	buffer[1] = '!';
-        cout << buffer << "==" << str_percent << ": " << repss_str::strcmp_ce(buffer,str_percent) << endl;
-        buffer[1] = '%';
-        cout << buffer << "==" << str_percent << ": " << repss_str::strcmp_ce(buffer,str_percent) << endl;
-        buffer[2] = '\0';
-        cout << buffer << "==" << str_percent << ": " << repss_str::strcmp_ce(buffer,str_percent) << endl;	
+    cout << buffer << "==" << str_percent << ": " << repss_str::strcmp_ce(buffer,str_percent) << endl;
+    buffer[1] = '%';
+    cout << buffer << "==" << str_percent << ": " << repss_str::strcmp_ce(buffer,str_percent) << endl;
+    buffer[2] = '\0';
+    cout << buffer << "==" << str_percent << ": " << repss_str::strcmp_ce(buffer,str_percent) << endl;
+
 	return 0;
 }
