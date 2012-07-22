909059d7b455cdd1531e538cdc2909aa11a548bb
khwillia <khalique.williams@gmail.com>
2012-07-22 02:13:32 -0400

    added some useful comments

--- src/includes/repss_str.hpp
+++ src/includes/repss_str.hpp
@@ -1,8 +1,29 @@
+/*
+	REPSS
+    Copyright (C) 2012  Khalique Williams
+
+    This file is part of REPSS.
+
+    REPSS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    REPSS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with REPSS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _REPSS_STR_
 #define _REPSS_STR_
 
 /**
- * namespace repss_str - Warning: Because of compiler restrictions, strings of a certain length may void evaluation of constexpr functions at compiletime
+ * namespace repss_str - Warning: Because of compiler restrictions, strings of a certain
+ *  length may void evaluation of constexpr functions at compile time
 */
 namespace repss_str
 {
